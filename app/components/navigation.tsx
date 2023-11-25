81eecb2a87eb240d8c503e0ddfa0a88b59b12ecc
sgmikami03 <sgmikami03@gmail.com>
2023-11-25 22:42:49 +0900

    [add]11 状態管理

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,10 +1,34 @@
 "use client";
 
 import Link from "next/link";
+import useStore from "@/store";
+import Image from "next/image";
+import { useEffect } from "react";
 import type { Session } from "@supabase/auth-helpers-nextjs";
+import type { Database } from "@/lib/database.types";
+type ProfileType = Database["public"]["Tables"]["profiles"]["Row"];
 
 // ナビゲーション
-const Navigation = ({ session }: { session: Session | null }) => {
+const Navigation = ({
+    session,
+    profile,
+}: {
+    session: Session | null;
+    profile: ProfileType | null;
+}) => {
+    const { setUser } = useStore();
+
+    // 状態管理にユーザー情報を保存
+    useEffect(() => {
+        setUser({
+            id: session ? session.user.id : "",
+            email: session ? session.user.email! : "",
+            name: session && profile ? profile.name : "",
+            introduce: session && profile ? profile.introduce : "",
+            avatar_url: session && profile ? profile.avatar_url : "",
+        });
+    }, [session, setUser, profile]);
+
     return (
         <header className="shadow-lg shadow-gray-100">
             <div className="py-5 container max-w-screen-sm mx-auto flex items-center justify-between">
@@ -16,7 +40,18 @@ const Navigation = ({ session }: { session: Session | null }) => {
                     {session ? (
                         <div className="flex items-center space-x-5">
                             <Link href="/settings/profile">
-                                <div>プロフィール</div>
+                                <div className="relative w-10 h-10">
+                                    <Image
+                                        src={
+                                            profile && profile.avatar_url
+                                                ? profile.avatar_url
+                                                : "/default.png"
+                                        }
+                                        className="rounded-full object-cover"
+                                        alt="avatar"
+                                        fill
+                                    />
+                                </div>
                             </Link>
                         </div>
                     ) : (
--- app/components/supabase-listener.tsx
+++ app/components/supabase-listener.tsx
@@ -14,7 +14,32 @@ const SupabaseListener = async () => {
         data: { session },
     } = await supabase.auth.getSession();
 
-    return <Navigation session={session} />;
+    // プロフィールの取得
+    let profile = null;
+    if (session) {
+        const { data: currentProfile } = await supabase
+            .from("profiles")
+            .select("*")
+            .eq("id", session.user.id)
+            .single();
+
+        profile = currentProfile;
+
+        // メールアドレスを変更した場合、プロフィールを更新
+        if (currentProfile && currentProfile.email !== session.user.email) {
+            // メールアドレスを更新
+            const { data: updatedProfile } = await supabase
+                .from("profiles")
+                .update({ email: session.user.email })
+                .match({ id: session.user.id })
+                .select("*")
+                .single();
+
+            profile = updatedProfile;
+        }
+    }
+
+    return <Navigation session={session} profile={profile} />;
 };
 
 export default SupabaseListener;
